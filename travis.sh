942147cc9a8a6140f62b003ba7e0bfc3b2103021
Lukáš Mešťan <arzzen@users.noreply.github.com>
2020-04-15 12:15:59 +0200

    Update travis.sh

--- travis.sh
+++ travis.sh
@@ -3,7 +3,5 @@ set -e # halt script on error
 
 echo 'Testing travis...'
 
-# bundle exec jekyll serve --config _config.yml,_config-dev.yml --watch
-
 bundle exec jekyll build
 bundle exec htmlproofer ./_site --only-4xx
